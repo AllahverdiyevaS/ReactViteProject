bef3b95d577cbd180ed0a294edaf99664c49d913
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-07-20 10:13:43 +0200

    Add styling for product card and student info components

--- src/Lesson1/ProductCard.jsx
+++ src/Lesson1/ProductCard.jsx
@@ -4,12 +4,15 @@ const ProductCard = () => {
   const url =
     "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-    <div>
-      <h2>Product name</h2>
-      <p>Product price</p>
-      <img src={url} alt={imgName} />
-      <a href={urlDecsription}>More Info</a>
-      <button>Buy</button>
+    <div className="container">
+      <h2>Notbook</h2>
+
+      <img src={url} alt={imgName} className="product_img" />
+      <p>100$</p>
+      <a href={urlDecsription} className="link">
+        More Info
+      </a>
+      <button className="button">Buy</button>
     </div>
   );
 };
--- src/Lesson1/StudentsInfo.jsx
+++ src/Lesson1/StudentsInfo.jsx
@@ -1,20 +1,26 @@
 const StudentsInfo = () => {
+  const languages = [
+    "Azerbaijani",
+    "Germany",
+    "English",
+    "Russians",
+    "Azerbaijani",
+    "Turkish",
+  ];
   const name = "Ali Aliyev";
   const firstSemestr = 91;
   const secondSemstr = 71;
-  const averageGradeForTheYear = (firstSemestr * secondSemstr) / 2;
+  const averageGradeForTheYear = (firstSemestr + secondSemstr) / 2;
   return (
     <div>
       <h2>Students Info</h2>
       <h3>Student name:{name}</h3>
-      students score:{averageGradeForTheYear}
+      <h3>students score: {averageGradeForTheYear}</h3>
       <h2>Language</h2>
       <ul>
-        <li>Azerbaijani</li>
-        <li>Germany</li>
-        <li>English</li>
-        <li>Russians</li>
-        <li>Turkish</li>
+        {languages.map((language) => (
+          <li>{language}</li>
+        ))}
       </ul>
     </div>
   );
