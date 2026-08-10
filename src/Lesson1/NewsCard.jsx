89202d24e06e6b872df0098be3e5a92e03cc9fc5
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-08-10 15:17:20 +0200

    Refactor NewsCard to use state for dynamic news addition

--- src/Lesson1/NewsCard.jsx
+++ src/Lesson1/NewsCard.jsx
@@ -1,53 +1,63 @@
 import NewsItem from "./NewsItem";
-
+import { useState } from "react";
 const NewsCard = () => {
-  const news = [
-    {
-      title: "News 1",
-      img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
-      link: "#",
-    },
-    {
-      title: "News 2",
-      img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
-      link: "#",
-    },
-    {
-      title: "News 3",
-      img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
-      link: "#",
-    },
-    {
-      title: "News 4",
-      img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
-      link: "#",
-    },
-    {
-      title: "News 5",
-      img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
-      link: "#",
-    },
-  ];
+  const [newInfos, setNewInfos] = useState([]);
+  function getRandomNews() {
+    const news = [
+      {
+        title: "News 1",
+        img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
+        link: "#",
+      },
+      {
+        title: "News 2",
+        img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
+        link: "#",
+      },
+      {
+        title: "News 3",
+        img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
+        link: "#",
+      },
+      {
+        title: "News 4",
+        img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
+        link: "#",
+      },
+      {
+        title: "News 5",
+        img: "https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "hdgfjkhlfrjidlsfjodlijdiolkjdsmncy b",
+        link: "#",
+      },
+    ];
+
+    return news[Math.floor(Math.random() * news.length)];
+  }
+  function handleClick() {
+    setNewInfos([...newInfos, getRandomNews()]);
+  }
 
   return (
-    <div className="news_container">
-      <h2>News</h2>
-
-      {news.map((item) => (
+    <>
+      <div className="news_container">
+        <h2>News</h2>
+      </div>
+      <button onClick={handleClick}>Add news</button>
+      {newInfos.map((item, index) => (
         <NewsItem
-          key={item}
+          key={index}
           title={item.title}
           img={item.img}
           description={item.description}
           link={item.link}
         />
       ))}
-    </div>
+    </>
   );
 };
 
